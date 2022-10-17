211eb0f0b64e8b4d6eef9a606ab6d88701637d0a
unknown <pantraj.deepak@gmail.com>
2022-10-17 17:37:46 +1100

    resolve conflict

--- app/Http/Controllers/SubNavItemsController.php
+++ app/Http/Controllers/SubNavItemsController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class SubNavItemsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
